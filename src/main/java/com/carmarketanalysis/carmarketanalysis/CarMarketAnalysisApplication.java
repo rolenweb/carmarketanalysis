faf39d9aba5a795f3d3067e680561c27cb1ecf92
Roman Khrustalev <rolenweb@yandex.ru>
2023-05-15 21:41:52 +0300

    Init project

---
+++ src/main/java/com/carmarketanalysis/carmarketanalysis/CarMarketAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.carmarketanalysis.carmarketanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarMarketAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarMarketAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carmarketanalysis/carmarketanalysis/CarMarketAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carmarketanalysis.carmarketanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMarketAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
